5734fff9f09785f71a76d94208d6ece6b73f8240
Mason Nesbitt <masonnesbitt@macbook-pro-10.local>
2018-09-24 13:06:35 -0500

    Initial Commit

---
+++ Guessing Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Guessing Game
+//
+//  Created by Mason Nesbitt on 9/24/18.
+//  Copyright © 2018 Mason Nesbitt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
