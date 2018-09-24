497b8bb058337c4d98603b1e742162b1140307ac
Mason Nesbitt <masonnesbitt@macbook-pro-10.local>
2018-09-24 13:44:22 -0500

    Initial Commit

--- Guessing Game/ViewController.swift
+++ Guessing Game/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var reset: UIButton!
+    @IBOutlet weak var triesRemaining: UILabel!
+    @IBOutlet weak var greetingLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var guessStatusLabel: UILabel!
+    @IBOutlet weak var submit: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
 
 }
 
